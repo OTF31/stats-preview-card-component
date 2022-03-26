2e34b592a2417d1a4bd63d1bb01028ac29feac3d
denis <182906@unsaac.edu.pe>
2022-03-26 00:56:33 -0500

    Add final code

--- src/App.js
+++ src/App.js
@@ -1,39 +1,52 @@
 const App = () => {
   return (
     <>
-      <div className="w-full h-screen flex items-center justify-center bg-[#090B1A] font-[Inter]">
-        <div className="my-auto w-[1080px] h-[446px] bg-[#1B1938] rounded-2xl">
+      <div className="w-full h-screen flex items-center justify-center bg-[#090B1A]">
+        <div className="my-auto w-[1010px] h-[446px] bg-[#1B1938] rounded-lg">
           <div className="flex h-full">
             {/* Left Side */}
-            <div className="w-[540px] text-white">
-              <h1>
+            <div className="w-[570px] text-white flex flex-col justify-evenly pl-[72px] pr-[108px]">
+              <h1 className="font-bold text-4xl font-[Inter] mt-[16.5px]">
                 Get <span className="text-[#AA5CDB]">insights</span> that help
                 your business grow.
               </h1>
-              <p className="text-[15px] text-gray-400">
+              <p className="text-[15px] text-[#FFFFFFC0] font-[Inter] relative bottom-[26px]">
                 Discover the benefits of data analytics and make better
                 decisions regarding revenue, customer experience, and overall
                 efficiency.
               </p>
-              <div className="flex justify-around">
-                <div>
-                  <p className="uppercase">10k+</p>
-                  <p className="uppercase text-gray-400">companies</p>
+              <div className="flex gap-[69px] relative bottom-[7px]">
+                <div className="flex flex-col">
+                  <p className="text-2xl font-bold font-[Inter]">10k+</p>
+                  <p className="uppercase text-[#FFFFFFC0] text-[12.5px] font-lexendDeca">
+                    companies
+                  </p>
                 </div>
-                <div>
-                  <p className="uppercase">314</p>
-                  <p className="uppercase text-gray-400">templates</p>
+
+                <div className="flex flex-col">
+                  <p className="uppercase text-2xl font-bold font-[Inter]">
+                    314
+                  </p>
+                  <p className="uppercase text-[#FFFFFFC0] text-[12.5px] font-lexendDeca">
+                    templates
+                  </p>
                 </div>
-                <div>
-                  <p className="uppercase">12m+</p>
-                  <p className="uppercase text-gray-400">queries</p>
+
+                <div className="flex flex-col">
+                  <p className="uppercase text-2xl font-bold font-[Inter]">
+                    12m+
+                  </p>
+                  <p className="uppercase text-[#FFFFFFC0] text-[12.5px] font-lexendDeca">
+                    queries
+                  </p>
                 </div>
               </div>
             </div>
 
             {/* Right Side */}
-            <div className="w-[540px] bg-[url('./assets/image-header-desktop.jpg')] rounded-r-2xl bg-no-repeat">
-              <div className="absolute w-[540px] h-[446px] bg-[#AA5CDB] opacity-[0.45] rounded-r-2xl"></div>
+            <div className="rounded-r-lg">
+              <div className="bg-[url('./assets/image-header-desktop.jpg')] w-[540px] h-[446px] absolute rounded-r-lg"></div>
+              <div className="absolute w-[540px] h-[446px] bg-[#AA5CDB] opacity-[0.45] rounded-r-lg"></div>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: { lexendDeca: ["Lexend Deca", "sans-serif"] },
     extend: {},
   },
   plugins: [],
