a7e323270d79f6e672b81e439f3b0ef07dfd97f1
denis <182906@unsaac.edu.pe>
2022-03-25 22:27:17 -0500

    Add center card and image at the rigth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import ImageHeaderDesktop from "./assets/image-header-desktop.jpg";
+
 const App = () => {
   return (
     <>
-      <h1>Hello</h1>
+      <div className="w-full h-screen flex items-center justify-center bg-[#090B1A]">
+        <div className="my-auto w-[1080px] h-[446px] bg-white">
+          <div className="flex h-full">
+            <div className="w-[540px]">left</div>
+            <div className="w-[540px] bg-[url('./assets/image-header-desktop.jpg')] bg-no-repeat">
+              <div className="absolute w-[540px] h-[446px] bg-red-800/25"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
