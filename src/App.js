83316db8dc88714b40204691d696b535a4eaf51e
denis <182906@unsaac.edu.pe>
2022-03-25 22:59:34 -0500

    Add bg color to the image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import ImageHeaderDesktop from "./assets/image-header-desktop.jpg";
-
 const App = () => {
   return (
     <>
       <div className="w-full h-screen flex items-center justify-center bg-[#090B1A]">
-        <div className="my-auto w-[1080px] h-[446px] bg-white">
+        <div className="my-auto w-[1080px] h-[446px] bg-[#1B1938] rounded-r-2xl">
           <div className="flex h-full">
-            <div className="w-[540px]">left</div>
-            <div className="w-[540px] bg-[url('./assets/image-header-desktop.jpg')] bg-no-repeat">
-              <div className="absolute w-[540px] h-[446px] bg-red-800/25"></div>
+            <div className="w-[540px] text-white">left</div>
+            <div className="w-[540px] bg-[url('./assets/image-header-desktop.jpg')] rounded-r-2xl bg-no-repeat">
+              <div className="absolute w-[540px] h-[446px] bg-[#AA5CDB] opacity-[0.45] rounded-r-2xl"></div>
             </div>
           </div>
         </div>
