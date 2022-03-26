502d4f33498f49817c157a9642e6474793f8b58e
denis <182906@unsaac.edu.pe>
2022-03-25 23:05:21 -0500

    Add texts

--- src/App.js
+++ src/App.js
@@ -4,7 +4,28 @@ const App = () => {
       <div className="w-full h-screen flex items-center justify-center bg-[#090B1A]">
         <div className="my-auto w-[1080px] h-[446px] bg-[#1B1938] rounded-r-2xl">
           <div className="flex h-full">
-            <div className="w-[540px] text-white">left</div>
+            <div className="w-[540px] text-white">
+              <h1> Get insights that help your business grow.</h1>
+              <p>
+                Discover the benefits of data analytics and make better
+                decisions regarding revenue, customer experience, and overall
+                efficiency.
+              </p>
+              <div className="flex justify-around">
+                <div>
+                  <p>10k+</p>
+                  <p>companies</p>
+                </div>
+                <div>
+                  <p>314</p>
+                  <p>templates</p>
+                </div>
+                <div>
+                  <p>12m+</p>
+                  <p>queries</p>
+                </div>
+              </div>
+            </div>
             <div className="w-[540px] bg-[url('./assets/image-header-desktop.jpg')] rounded-r-2xl bg-no-repeat">
               <div className="absolute w-[540px] h-[446px] bg-[#AA5CDB] opacity-[0.45] rounded-r-2xl"></div>
             </div>
